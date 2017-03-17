ac0c343e6e44b423b28edcac99a96ab6ac5dd7d9
thinsoldier <thinsoldier@thinsoldier.com>
2017-03-17 00:23:01 -0600

    Move all the PHP above the form html.
    Just in case you decide to do some header() stuff later or if you decide you don't want to show the form at all after a successful signup has occurred.

--- register.php
+++ register.php
@@ -1,53 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Signup Page</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-	<div class="container">
-		<h2>
-			Loud Gobs Browser Signup Form
-		</h2>
-		<form method="post" action="">
-			<div class="form-group">
-				<label for="username">Username:</label> 
-				<input type="text" class="form-control" id="user" placeholder="Enter a unique Username" name="member_registration_username">
-			</div>
-			<div class="form-group">
-				<label for="password">Password:</label> 
-				<input type="password" class="form-control" id="pwd" placeholder="Enter new Password" name="member_registration_password">
-			</div>
-			<div class="form-group">
-				<label for="password">Repeat Password:</label> 
-				<input type="password" class="form-control" id="member_registration_repeat_pwd" placeholder="Repeat new Password" name="member_registration_password_confirmation">
-			</div>
-			<div class="form-group">
-				<label for="forename">First Name:</label> 
-				<input type="text" class="form-control" id="member_registration_first_name" placeholder="Enter your First Name" name="member_registration_forename">
-			</div>
-			<div class="form-group">
-				<label for="surname">Surname:</label> 
-				<input type="text" class="form-control" id="member_registration_last_name" placeholder="Enter your Surname" name="member_registration_surname">
-			</div>
-			<div class="form-group">
-				<label for="email">Email:</label> 
-				<input type="email" class="form-control" id="member_registration_email" placeholder="Enter your Email" name="member_registration_email">
-			</div>
-			<div class="form-group">
-				<label for="email">Repeat Email:</label> 
-				<input type="email" class="form-control" id="member_registration_repeat_email" placeholder="Repeat your Email" name="member_registration_email_confirmation">
-			</div>
-			<button type="submit" class="btn btn-default" name="submit">Register!</button> <b>Already have an account ?</b><br>
-			<a href="login.php">Login here!</a> 
-		</form>
-	</div>
-</body>
-</html>
 <?php
 require "conn.php";
 if  (isset($_POST['submit']))
@@ -157,4 +107,53 @@ if  (isset($_POST['submit']))
         }
 }
 
-?>
+?><!DOCTYPE html>
+<html>
+<head>
+<title>Signup Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<h2>
+			Loud Gobs Browser Signup Form
+		</h2>
+		<form method="post" action="">
+			<div class="form-group">
+				<label for="username">Username:</label> 
+				<input type="text" class="form-control" id="user" placeholder="Enter a unique Username" name="member_registration_username">
+			</div>
+			<div class="form-group">
+				<label for="password">Password:</label> 
+				<input type="password" class="form-control" id="pwd" placeholder="Enter new Password" name="member_registration_password">
+			</div>
+			<div class="form-group">
+				<label for="password">Repeat Password:</label> 
+				<input type="password" class="form-control" id="member_registration_repeat_pwd" placeholder="Repeat new Password" name="member_registration_password_confirmation">
+			</div>
+			<div class="form-group">
+				<label for="forename">First Name:</label> 
+				<input type="text" class="form-control" id="member_registration_first_name" placeholder="Enter your First Name" name="member_registration_forename">
+			</div>
+			<div class="form-group">
+				<label for="surname">Surname:</label> 
+				<input type="text" class="form-control" id="member_registration_last_name" placeholder="Enter your Surname" name="member_registration_surname">
+			</div>
+			<div class="form-group">
+				<label for="email">Email:</label> 
+				<input type="email" class="form-control" id="member_registration_email" placeholder="Enter your Email" name="member_registration_email">
+			</div>
+			<div class="form-group">
+				<label for="email">Repeat Email:</label> 
+				<input type="email" class="form-control" id="member_registration_repeat_email" placeholder="Repeat your Email" name="member_registration_email_confirmation">
+			</div>
+			<button type="submit" class="btn btn-default" name="submit">Register!</button> <b>Already have an account ?</b><br>
+			<a href="login.php">Login here!</a> 
+		</form>
+	</div>
+</body>
+</html>
